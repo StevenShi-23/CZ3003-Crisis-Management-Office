27331e2077b2c09628eda561ab506ed93d0bd748
ng shikai <shikai@ngs-MacBook-Pro.local>
2017-10-09 13:41:33 +0800

    rename CMO

---
+++ CMO/CMOBackend/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponse, HttpResponseRedirect
+
+
+def index(request):
+    return render(request,'CMOBackend/index.html')
