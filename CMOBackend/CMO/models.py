86a196c1b21e83b8b28ce473878612a14b5e031b
ng shikai <shikai@ngs-MacBook-Pro.local>
2017-10-09 13:21:36 +0800

    backend init

---
+++ CMOBackend/CMO/models.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+#     def __str__ (self):
+#         return self.question_text
