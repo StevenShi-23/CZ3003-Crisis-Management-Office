adce362b826a34d482b7c263e27d57320a919c67
ng shikai <shikai@ngs-MacBook-Pro.local>
2017-10-11 20:10:29 +0800

    pre merge

--- CMOSite/CMOBackend/models.py
+++ CMOSite/CMOBackend/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 import datetime
 
@@ -23,22 +24,30 @@ class Call(models.Model):
 	ContactPersonName = models.CharField(max_length=200, blank=True)
 	ContactPersonNumber = models.IntegerField(blank=True)
 	Datetime = models.DateTimeField('date time received')
-	BriefDescription = models.CharField(max_length=25)
+	BriefDescription = models.TextField()
 	def __str__(self):
 		return self.BriefDescription
 
-# @python_2_unicode_compatible
+@python_2_unicode_compatible
 class Plan(models.Model):
+	TERRORIST = 'TR'
+	NATURAL_DISASTER = 'ND'
+	EPIDEMIC = 'EP'
+    CRISIS_CHOICES = (
+        (TERRORIST, 'Terrorist'),
+        (NATURAL_DISASTER, 'Natural Disaster'),
+        (EPIDEMIC, 'Epidemic'),
+    )
 	CrisisID = models.ForeignKey(Crisis, on_delete=models.CASCADE)
 	PlanID = models.IntegerField(primary_key=True)
 	isApprovedByPMO = models.BooleanField(default=False, blank=True)
 	isApprovedByGeneral = models.BooleanField(default=False, blank=True)
 	Datetime = models.DateTimeField('date time of plan submission')
-	CrisisType = models.CharField(max_length=200)
-	AnalysisOfCase = models.CharField(max_length=255)
+	CrisisType = models.CharField(max_length=2, choices=CRISIS_CHOICES)
+	AnalysisOfCase = models.TextField()
 	Map = models.URLField('map url')
 
-# @python_2_unicode_compatible
+@python_2_unicode_compatible
 class SuggestedActions(models.Model):
 
 	MILITARY = 'MIL'
@@ -68,9 +77,9 @@ class SuggestedActions(models.Model):
 	TypeTroop = models.CharField(
 		max_length=3,
 		choices=TROOP_CHOICES)
-	SeverityLevel = models.IntegerField()
+	SeverityLevel = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
 
-# @python_2_unicode_compatible
+@python_2_unicode_compatible
 class Update(models.Model):
 	UpdateID = models.IntegerField(primary_key=True)
 	CrisisID = models.ForeignKey(Crisis, on_delete=models.CASCADE)
@@ -78,4 +87,4 @@ class Update(models.Model):
 	Status = models.CharField(max_length=10)
 	Comment = models.CharField(max_length=255)
 	Location = models.CharField(max_length=200)
-	SeverityLevel = models.IntegerField()
+	SeverityLevel = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
